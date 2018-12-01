1f69cdb8e735d778d8f305b78d01fd9d61d4742f
Gurbx <philip.lindberg101@gmail.com>
2018-12-01 10:58:09 +0200

    Movement

---
+++ Ludum Dare 43/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	[SerializeField] private float mouseSensitivity;
+	[SerializeField] private Transform playerTransform;
+
+	private float xClamp = 0;
+
+	void Start () {
+		xClamp = 0;
+	}
+
+	void Update () {
+		HandleRotation ();
+	}
+
+	private void HandleRotation() {
+		float mouseX = Input.GetAxis ("Mouse X") * mouseSensitivity * Time.deltaTime;
+		float mouseY = Input.GetAxis ("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+		xClamp += mouseY;
+		if (xClamp < -90f) {
+			xClamp = -90f;
+			mouseY = 0;
+			ClampXAxisRotationToValue (90f);
+		} else if (xClamp > 90f) {
+			xClamp = 90f;
+			mouseY = 0;
+			ClampXAxisRotationToValue (270f);
+		}
+			
+		transform.Rotate (Vector3.left * mouseY);
+		playerTransform.Rotate (Vector3.up * mouseX);
+	}
+
+
+	private void ClampXAxisRotationToValue(float value)
+	{
+		Vector3 eulerRotation = transform.eulerAngles;
+		eulerRotation.x = value;
+		transform.eulerAngles = eulerRotation;
+	}
+
+}
---
+++ Ludum Dare 43/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField] private float movementSpeed = 10;
+	[SerializeField] private float gravity = 240f;
+
+	private Vector3 moveDirection = Vector3.zero;
+	private CharacterController charController;
+
+	void Start () {
+		charController = GetComponent<CharacterController> ();
+	}
+
+	void Update () {
+		//Movement
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, (Input.GetAxis("Vertical")));
+		moveDirection = transform.TransformDirection (moveDirection);
+		moveDirection *= movementSpeed;
+		moveDirection = Vector3.ClampMagnitude (moveDirection, movementSpeed);
+
+		moveDirection.y -= gravity * Time.deltaTime;
+		charController.Move (moveDirection * Time.deltaTime);
+	}
+}

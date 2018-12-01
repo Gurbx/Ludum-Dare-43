e24d73ca21f33c38519465e7eb9e397c378eceef
Gurbx <philip.lindberg101@gmail.com>
2018-12-01 11:39:18 +0200

    Start on inventory

---
+++ Ludum Dare 43/Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+	private InventorySlot[] slots;
+	private int selectedIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		slots = GetComponentsInChildren<InventorySlot> ();
+	}
+
+	void Update () {
+		//Key input
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			deselectAll ();
+			slots [0].SetSelected (true);
+			selectedIndex = 0;
+		}
+
+		MouseScroll ();
+			
+	}
+
+	private void MouseScroll() {
+		if (Input.GetAxis ("Mouse ScrollWheel") > 0f) {
+			selectedIndex += 1;
+			if (selectedIndex > slots.GetLength (0)-1)
+				selectedIndex = 0;
+			else if (selectedIndex < 0)
+				selectedIndex = slots.GetLength (0)-1;
+
+			deselectAll ();
+			slots [selectedIndex].SetSelected (true);
+		}
+
+		if (Input.GetAxis ("Mouse ScrollWheel") < 0f) {
+			selectedIndex -= 1;
+			if (selectedIndex > slots.GetLength (0)-1)
+				selectedIndex = 0;
+			else if (selectedIndex < 0)
+				selectedIndex = slots.GetLength (0)-1;
+
+			deselectAll ();
+			slots [selectedIndex].SetSelected (true);
+		}
+	}
+
+
+	private void deselectAll() {
+		for (int i = 0; i < slots.GetLength (0); i++) {
+			slots [i].SetSelected (false);
+		}
+	}
+}
--- Ludum Dare 43/Assets/Scripts/Inventory/InventorySlot.cs
+++ Ludum Dare 43/Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,20 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour {
 
 	private UsableItem item;
 	private bool isSelected;
 	private bool isEmpty;
+	private Image bg;
 
 	// Use this for initialization
 	void Start () {
-		
+		bg = GetComponent<Image> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (Input.GetMouseButtonDown (0) && isSelected && !isEmpty) {
+			//item.UseItem ();
+		}
+	}
 		
+	public void SetSelected(bool selected) {
+		isSelected = selected;
+		if (isSelected) {
+			bg.color = Color.white;
+		} else {
+			bg.color = Color.gray;
+		}
 	}
 }
